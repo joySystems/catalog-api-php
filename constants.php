5d82d79ac7cbc86e84603778717a708fa4474cbb
JoySystem_v <fvv2011@gmail.com>
2023-05-14 11:36:30 +0400

    added constants file

---
+++ constants.php
@@ -0,0 +1,15 @@
+<?php
+
+
+// Данные авторизации
+define('USERNAME', "labsales_test");
+define('PASSWORD', "18765gR5");
+
+// Адреса URL API
+define("CATS_URL", "https://test.labsales.ru/tasks/articles/rest/categories");
+
+
+
+
+
+?>
--- index.php
+++ index.php
@@ -1,19 +1,25 @@
 <?php
-$user = "labsales_test";
-$pass = "18765gR5";
+
+require_once('constants.php');
+
+$user = USERNAME;
+$pass = PASSWORD;
+$categories_url = CATS_URL;
+
 
 $ch = curl_init();
 
-curl_setopt($ch, CURLOPT_URL, "https://test.labsales.ru/tasks/articles/rest/categories");
+curl_setopt($ch, CURLOPT_URL, $categories_url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_USERPWD, $user.":". $pass);
 
 $result = curl_exec($ch);
 
 if ($e = curl_error($ch)) {
+    echo "ответ curl:". '<br>';
     echo $e;
 }else{
-
+    echo "ответ сервера:". '<br>';
     $data_json = json_decode($result, true);
     var_dump($data_json);
 }
